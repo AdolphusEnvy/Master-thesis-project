5d32d58971dd83572fbd5030df52a70db88add3f
root <hy8227852@163.com>
2020-03-15 23:43:40 +0000

    makefile compile C++ component

---
+++ Ibis-application/src/main/cpp/Driver.cpp
@@ -0,0 +1,15 @@
+// Save as "HelloJNI.cpp"
+#include <jni.h>       // JNI header provided by JDK
+#include <iostream>    // C++ standard IO header
+#include <string>
+#include "Driver.h"  // Generated
+using namespace std;
+
+// Implementation of the native method sayHello()
+JNIEXPORT jint JNICALL Java_Driver_sayHello(JNIEnv *env, jobject thisObj,jstring inJNIStr) {
+ const char *inCStr = env->GetStringUTFChars(inJNIStr, NULL);
+    //if (NULL == inCStr) return NULL;
+    ///const char *inCStr="123";
+	cout << "Hello World from C++!"<<endl<< "The file is "<<inCStr<< endl;
+   return 1;
+}
